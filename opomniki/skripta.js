9b484f72b9914e38e4c2a22295ea03c2b886d35e
cejp <peter.cej.sov@gmail.com>
2016-03-10 10:55:46 +0000

    Implementirana prijava

--- opomniki/skripta.js
+++ opomniki/skripta.js
@@ -1,15 +1,26 @@
 window.addEventListener('load', function() {
 	//stran nalozena
 		
+		
+	//Koda za prijavo
+	var izvediPrijavo = function(event){
+		var uporabnik = document.querySelector("#uporabnisko_ime").value;
+		document.querySelector("#uporabnik").innerHTML = uporabnik;
+		document.querySelector(".pokrivalo").style.visibility="hidden";
+	}
+	document.querySelector("#prijavniGumb").addEventListener('click',izvediPrijavo);
+	
 	//Posodobi opomnike
 	var posodobiOpomnike = function() {
 		var opomniki = document.querySelectorAll(".opomnik");
+		var i;
 		
 		for (i = 0; i < opomniki.length; i++) {
 			var opomnik = opomniki[i];
 			var casovnik = opomnik.querySelector("span");
 			var cas = parseInt(casovnik.innerHTML);
 	
+			
 			//TODO: 
 			// - če je čas enak 0, izpiši opozorilo "Opomnik!\n\nZadolžitev NAZIV_OPOMNIK je potekla!"
 			// - sicer zmanjšaj čas za 1 in nastavi novo vrednost v časovniku
